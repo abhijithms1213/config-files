f3aa913e3cc090dbe808684b475c519235faf5a0
abhijithms1213 <abhijithms1213@gmail.com>
2025-07-13 16:53:00 +0530

    nvim added initial stage

---
+++ .config/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,11 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    { "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", desc = "Tmux Navigate Left" },
+    { "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", desc = "Tmux Navigate Down" },
+    { "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", desc = "Tmux Navigate Up" },
+    { "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", desc = "Tmux Navigate Right" },
+    { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", desc = "Tmux Navigate Previous" },
+  },
+}
